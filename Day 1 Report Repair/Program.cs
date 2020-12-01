a5d8a93eb427fc13d13491e71de88931cc6f0764
sai wentum <michael.wentum@gmail.com>
2020-12-01 07:24:04 -0500

    Add project files.

---
+++ Day 1 Report Repair/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day_1_Report_Repair
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
