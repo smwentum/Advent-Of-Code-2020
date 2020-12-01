5b3a18e9c3ff72a56fb6718edd98a092419cc92d
sai wentum <michael.wentum@gmail.com>
2020-12-01 10:45:54 -0500

    did part 2

--- Day 1 Report Repair/Part1.cs
+++ Day 1 Report Repair/Part1.cs
@@ -7,11 +7,11 @@ namespace Day_1_Report_Repair
     public class Part1
     {
         /// <summary>
-        /// For part one i just need to find the numbers that add up to 2020 and print out the product
+        /// For part one i just need to find two numbers that add up to 2020 and print out the product
         /// now that i think about it more it wont work right 1010
         /// </summary>
         /// <param name="args"></param>
-        static void Main(string[] args)
+        static void Main1(string[] args)
         {
             List<int> expenses = new List<int>();
             foreach (string line in File.ReadLines(@"D:\Documents\random programming stuff\Advent of code\2020\Advent Of Code 2020\Day 1 Report Repair\part1data.txt"))
---
+++ Day 1 Report Repair/Part2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Day_1_Report_Repair
+{
+    public class Part2
+    {
+        /// <summary>
+        /// For part one i just need to find three numbers that add up to 2020 and print out the product
+        /// going to triple for loop it  but there is probably something more clever i am not thinking about
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            List<int> expenses = new List<int>();
+            foreach (string line in File.ReadLines(@"D:\Documents\random programming stuff\Advent of code\2020\Advent Of Code 2020\Day 1 Report Repair\part2data.txt"))
+            {
+                expenses.Add(int.Parse(line));
+            }
+
+
+            for (int i = 0; i < expenses.Count; i++)
+            {
+                for (int j = i+1; j < expenses.Count; j++)
+                {
+                    for (int k = j+1; k < expenses.Count; k++)
+                    {
+                        if (expenses[i] + expenses[j] + expenses[k] == 2020)
+                        {
+                            Console.WriteLine(expenses[i]);
+                            Console.WriteLine(expenses[j]);
+                            Console.WriteLine(expenses[k]);
+                            Console.WriteLine(expenses[i] + expenses[j] + expenses[k]);
+                            Console.WriteLine(expenses[i] * expenses[j] * expenses[k]);
+                            break;
+                        }
+                    }
+                }
+            }
+           
+
+        }
+    }
+}
