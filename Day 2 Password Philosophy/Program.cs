83267dced51760cc0c5074db9bf92421bbda7027
sai wentum <michael.wentum@gmail.com>
2020-12-02 06:12:38 -0500

    starting day 2

---
+++ Day 2 Password Philosophy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day_2_Password_Philosophy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
