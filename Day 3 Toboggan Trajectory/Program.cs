44aa7f1b10fb7c6584a3c416c744d4c95d9d371f
sai wentum <michael.wentum@gmail.com>
2020-12-03 07:38:41 -0500

    did part 2

--- Day 3 Toboggan Trajectory/Program.cs
+++ Day 3 Toboggan Trajectory/Program.cs
@@ -12,39 +12,66 @@ namespace Day_3_Toboggan_Trajectory
             string path = @"D:\Documents\random programming stuff\Advent of code\2020\Advent Of Code 2020\Day 3 Toboggan Trajectory\real.txt";
 
             List<string> field = new List<string>();
-          
-                field = File.ReadAllLines(path).ToList();
 
-            field = expandField(field);
-
-            int count = 0;
-            int i = 0; 
-            int j = 0;
+            long ans = 1; 
             
+            field = expandFieldFactor(File.ReadAllLines(path).ToList(), 7, 1);
+            Console.WriteLine(computeAns(field,1,1));
+            ans *= computeAns(field, 1, 1);
+
+            field = expandFieldFactor(File.ReadAllLines(path).ToList(), 3, 1);
+            Console.WriteLine(computeAns(field,3,1));
+            ans *= computeAns(field,3,1);
+
+            field = expandFieldFactor(File.ReadAllLines(path).ToList(), 5, 1);
+            Console.WriteLine(computeAns(field, 5, 1));
+            ans *= computeAns(field,5,1);
+
+
+            field = expandFieldFactor(File.ReadAllLines(path).ToList(), 7, 1);
+            Console.WriteLine(computeAns(field, 7, 1));
+            ans *= computeAns(field,7,1);
+
+
+            field = expandFieldFactor(File.ReadAllLines(path).ToList(), 7, 2);
+            Console.WriteLine(computeAns(field,1, 2));
+            ans *= computeAns(field,1,2);
+
+
+
+            Console.WriteLine(ans);
+        }
+
+        private static int computeAns(List<string> field, int right, int down)
+        {
+            int count = 0;
+            int i = 0;
+            int j = 0;
+
             while (i < field.Count && j < field[0].Length)
             {
 
                 if (field[i][j] == '#')
                 {
                     count++;
-                    Console.WriteLine(field[i][j] + $" i = {i} j= {j}");
+                    //Console.WriteLine(field[i][j] + $" i = {i} j= {j}");
                 }
                 else
                 {
-                    Console.WriteLine(field[i][j] +$" i = {i} j= {j}");
+                    //Console.WriteLine(field[i][j] + $" i = {i} j= {j}");
                 }
-                i  += 1;
-                 j += 3;
-                
-            }
-            Console.WriteLine(count);
+                i += down;
+                j += right;
 
+            }
+            //Console.WriteLine(count);
+            return count;
         }
 
-        private static List<string> expandField(List<string> field)
+        private static List<string> expandFieldFactor(List<string> field, int right, int left)
         {
             
-            while ( 3*field.Count > field[0].Length)
+            while ( right*field.Count > left*field[0].Length)
             {
                 for (int i = 0; i < field.Count; i++)
                 {
