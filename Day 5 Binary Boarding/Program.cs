9cc1cd3f398873bf102bad00d971601f2a16b86c
sai wentum <michael.wentum@gmail.com>
2020-12-05 10:23:43 -0500

    did part 2

--- Day 5 Binary Boarding/Program.cs
+++ Day 5 Binary Boarding/Program.cs
@@ -7,17 +7,33 @@ namespace Day_5_Binary_Boarding
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             string path = @"D:\Documents\random programming stuff\Advent of code\2020\Advent Of Code 2020\Day 5 Binary Boarding\real.txt";
+
             //read in the text
             List<string> boardingPasses = File.ReadAllLines(path).ToList();
             long maxValue = -1;
+            bool[] seats = new bool[1000];
+            for(int i = 0; i < seats.Count(); i++)
+            {
+                seats[i] = true;
+            }
             foreach (string boardingPass in boardingPasses)
             {
+                seats[(int)getSeatId(boardingPass)] = false;
                 maxValue = Math.Max(maxValue, getSeatId(boardingPass));
             }
+           
             Console.WriteLine(maxValue);
+            for (int i = 2; i < seats.Count(); i++)
+            {
+                if (!seats[i - 1] && seats[i] && !seats[i + 1])
+                {
+                    Console.WriteLine(i);
+                }
+            }
         }
 
         private static long getSeatId(string boardingPass)
